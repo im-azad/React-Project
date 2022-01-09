d271909b70400e42fed7d478a858bba13e91d028
Abul Kalam Azad <me.azad99@gmail.com>
2022-01-09 21:51:48 +0600

    css stylin in jsx

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+// import './index.css';
+const inlineStyle = {
+	color: 'red',
+};
 ReactDOM.render(
-	<h1 style={{ textAlign: 'center' }}>Hello, Welcome to React world.</h1>,
+	<h1 className='greeting' style={inlineStyle}>
+		Hello, Welcome to React world.
+	</h1>,
 	document.getElementById('root')
 );
