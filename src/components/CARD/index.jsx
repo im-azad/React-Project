2066135c9f650ee4c161b0151d47d79968cbbc37
Abul Kalam Azad <me.azad99@gmail.com>
2022-01-11 21:13:45 +0600

    use class component

--- src/components/CARD/index.jsx
+++ src/components/CARD/index.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
 import './card.css';
 
+// import React from 'react'; // Functional component
+/* 
 function Card(props) {
 	const { cardTitle, cardFooter } = props;
 	return (
@@ -15,4 +16,24 @@ function Card(props) {
 	);
 }
 
-export default Card;
+export default Card; */
+
+// Class component
+import React, { Component } from 'react';
+
+export default class Card extends Component {
+	render() {
+		return (
+			<div>
+				<div className='card-body'>
+					<h3 className='card-title'>{this.props.cardTitle}</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit
+						harum repellendus consequatur voluptate minus.
+					</p>
+					<p className='card-footer'>{this.props.cardFooter}</p>
+				</div>
+			</div>
+		);
+	}
+}
